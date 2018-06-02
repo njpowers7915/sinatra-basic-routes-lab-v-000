b2fd7a9a03b49b94949309e9eff6c4df4f52e7fa
Nicholas Powers <njpowers7915@gmail.com>
2018-06-02 02:32:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Nick."
+  end
+  
+  get '/hometown' do
+    "My hometown is Wyckoff."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'Call on Me'."
+  end
 end
